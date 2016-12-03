17981b8df6af4e1076a840d3044375a13cbb7577
Stephen Clibbery <sdclibbery@googlemail.com>
2016-12-03 08:53:30 +0000

    Parsing input

--- day-2-1.hs
+++ day-2-1.hs
@@ -1,6 +1,15 @@
+import Prelude hiding (Left, Right)
 
 main = do
         input <- readFile "day-2.test.data"
         print $ process input
 
-process = lines
+data Instruction = Left | Right | Up | Down | Press deriving (Show, Read)
+
+process = map parse
+
+parse '\n' = Press
+parse 'L' = Left
+parse 'R' = Right
+parse 'U' = Up
+parse 'D' = Down
