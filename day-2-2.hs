099070b92dd4d7b88e33ba7a5632f4738e3d7301
Stephen Clibbery <sdclibbery@googlemail.com>
2016-12-04 10:52:29 +0000

    Working version using a button grid

--- day-2-2.hs
+++ day-2-2.hs
@@ -1,13 +1,34 @@
+import Control.Lens
+import Data.Maybe
+import Control.Monad
 
 main = do
-        input <- readFile "day-2.test.data"
+        input <- readFile "day-2.data"
         print $ process input
 
 data Instruction = L | R | U | D | P deriving (Show, Read)
 data Coords = Coords { xPos::Int, yPos::Int } deriving (Show)
-data Action = MoveTo Coords | Press Coords deriving (Show)
+data Action = MoveTo { pos::Coords } | Press { pos::Coords } deriving (Show)
 
-process = concat . map button . filter presses . foldl walk [MoveTo (Coords 3 3)] . map parse
+inputButtonGrid = [ "    ",
+                    "   1 ",
+                    "  234 ",
+                    " 56789 ",
+                    "  ABC ",
+                    "   D ",
+                    "    " ]
+
+buttonGrid :: [[Maybe Char]]
+buttonGrid = map parseRow $ inputButtonGrid
+  where
+    parseRow = map (\c -> if c == ' ' then Nothing else Just c)
+
+process = map fromJust
+          . map buttonAt
+          . map pos
+          . filter presses
+          . foldl walk [MoveTo (Coords 1 3)]
+          . map parse
 
 parse '\n' = P
 parse c = read [c]
@@ -15,27 +36,24 @@ parse c = read [c]
 walk :: [Action] -> Instruction -> [Action]
 walk as i = as ++ [apply (pos (last as)) i]
 
-pos (MoveTo c) = c
-pos (Press c) = c
-
 apply :: Coords -> Instruction -> Action
 apply c P = Press c
-apply c _ = MoveTo c -- WriteMe!
+apply c L = move c (-1) 0
+apply c R = move c 1 0
+apply c U = move c 0 (-1)
+apply c D = move c 0 1
+
+move :: Coords -> Int -> Int -> Action
+move c@(Coords x y) dx dy = MoveTo $ if isNothing next then c else target
+  where
+    target = Coords (x+dx) (y+dy)
+    next = buttonAt target
 
 presses (Press _) = True
 presses _ = False
 
-button (Press (Coords 3 1)) = "1"
-button (Press (Coords 2 2)) = "2"
-button (Press (Coords 3 2)) = "3"
-button (Press (Coords 4 2)) = "4"
-button (Press (Coords 1 3)) = "5"
-button (Press (Coords 2 3)) = "6"
-button (Press (Coords 3 3)) = "7"
-button (Press (Coords 4 3)) = "8"
-button (Press (Coords 5 3)) = "9"
-button (Press (Coords 2 4)) = "A"
-button (Press (Coords 3 4)) = "B"
-button (Press (Coords 4 4)) = "C"
-button (Press (Coords 3 5)) = "D"
-button _ = "?"
+buttonAt :: Coords -> Maybe Char
+buttonAt (Coords x y) = do
+  maybeButtonRow <- buttonGrid ^? element y
+  maybeButton <- join $ maybeButtonRow ^? element x
+  return maybeButton
