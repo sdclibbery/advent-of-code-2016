e32a8200e74a774ed83824f79fadb8930f761de9
Stephen Clibbery <stephen.clibbery@thehutgroup.com>
2016-12-05 18:13:58 +0000

    Read the instructions and summed the ids, not got the count!!!

--- day-4-1.hs
+++ day-4-1.hs
@@ -6,23 +6,25 @@ main = do
         input <- readFile "day-4.data"
         print $ countRealRooms input
 
-data Room = Room { name::String, checksum::String } deriving (Show)
+data Room = Room { name::String, sector::Int, checksum::String } deriving (Show)
 
 countRealRooms = id
-                  .length
+                  .sum
+                  .map sector
                   -- .map (calcChecksum.name)
                   .filter isValidRoom
                   .map parseRoom
                   .lines
 
 parseRoom :: String -> Room
-parseRoom s = Room name checksum
+parseRoom s = Room name sector checksum
   where
     name = filter isLetter $ takeWhile (/= '[') s
+    sector = read $ filter isDigit s
     checksum = reverse $ filter isLetter $ takeWhile (/= '[') $ reverse s
 
 isValidRoom :: Room -> Bool
-isValidRoom (Room n c) = (c == calcChecksum n)
+isValidRoom (Room n _ c) = (c == calcChecksum n)
 
 calcChecksum :: String -> String
 calcChecksum = map fst . take 5 . sortBy compareByCountThenAlphabetically . map counts . group . sort
