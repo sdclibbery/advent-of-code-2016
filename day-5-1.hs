60f8f25da4dca24ee0b2bc27a25325c4fe060874
Stephen Clibbery <stephen.clibbery@thehutgroup.com>
2016-12-06 13:53:00 +0000

    Eta reduce a lambda

--- day-5-1.hs
+++ day-5-1.hs
@@ -7,6 +7,6 @@ input = "ojvtpuvg"
 main = print $ password input
 
 password :: String -> String
-password s = (map (!! 5) . take 8 . filter (isPrefixOf "00000") . map generateHash . map (\v -> s++v) . map show) [1..]
+password s = (map (!! 5) . take 8 . filter (isPrefixOf "00000") . map generateHash . map (s++) . map show) [1..]
 
 generateHash = md5s . Str
