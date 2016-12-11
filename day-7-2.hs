3b86f0d8499c32e7876ce94a04ff2552a174c953
Stephen Clibbery <sdclibbery@googlemail.com>
2016-12-11 23:19:12 +0000

    Finding Abas

--- day-7-2.hs
+++ day-7-2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 import Text.Parsec
 import Text.Parsec.Char
 
@@ -6,19 +8,28 @@ main = do
 --        print $ (length.getSsl) input
         print $ getSsl input
 
-data Component = Supernet String | Hypernet String deriving (Show)
+data Component a = Supernet a | Hypernet a deriving (Show, Functor)
+data Aba = Aba Char Char deriving (Show)
 
-getSsl = map parseIP
+getSsl = map (map (fmap findAbas))
+          .map parseIP
           .lines
 
-parseIP :: String -> [Component]
+parseIP :: String -> [Component String]
 parseIP s = right $ parse iPparser "day-7" s
   where
     right (Right x) = x
 
-iPparser :: Parsec String st [Component]
+iPparser :: Parsec String st [Component String]
 iPparser = many (supernetParser <|> hypernetParser)
   where
     supernetParser = Supernet <$> text
     hypernetParser = Hypernet <$> ((char '[') *> text <* (char ']'))
     text = many1 $ noneOf "[]"
+
+findAbas :: String -> [Aba]
+findAbas = foldl extract [] . triples
+  where
+    triples s = zip3 (tail $ tail s) (tail s) s
+    extract xs (a,b,c) | (a==c && a/=b) = (Aba a b) : xs
+    extract xs _ = xs
